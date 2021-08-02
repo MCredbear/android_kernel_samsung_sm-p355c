35f24ef2f9f4fc9bb5105b19c91152b236917c1a
MCredbear <2363764471@qq.com>
2021-08-02 13:18:38 +0800

    Download from https://opensource.samsung.com/ (Where this source code will be unloaded soon)

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Apr 8 11:28:10 KST 2015"
+#define LINUX_COMPILE_BY "dao.zheng"
+#define LINUX_COMPILE_HOST "R0709-09"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
